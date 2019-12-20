745387ff8249fbc06be4f6cc3b6378c833003a5d
王劲鹏 <jingpeng.wang@outlook.com>
2019-12-20 11:35:16 +0800

    initial project with cli

---
+++ android/app/src/main/java/pub/doric/example/MainActivity.java
@@ -0,0 +1,24 @@
+package pub.doric.example;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pub.doric.DoricFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private final String BUNDLE_NAME = "doriclib-media";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            String scheme = "assets://" + BUNDLE_NAME + ".js";
+            getIntent().putExtra("scheme", scheme);
+            getIntent().putExtra("alias", BUNDLE_NAME);
+            this.getSupportFragmentManager().beginTransaction().add(R.id.root, new DoricFragment()).commit();
+        }
+    }
+}
